727aa6d2ede397e0f3daf3bb8f783adfd339963d
chinpeerapat <peerapat.chiaprasert@gmail.com>
2024-09-27 02:17:02 +0700

    fix

--- app/main.py
+++ app/main.py
@@ -7,7 +7,3 @@ app = FastAPI()
 @app.get("/get/{phone_number}", response_model=ScrapedResponse)
 async def scrape_endpoint(phone_number: str):
     return await scrape_content(phone_number)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="$HOST", port="$PORT")
